88d02858f0b4bd5689087ad62fd50429d4c065af
sonali-kumari1505 <sonalisinghrajput1498@gmail.com>
2025-03-16 11:17:52 +0530

    udate files

---
+++ canvas.js
@@ -0,0 +1,13 @@
+let x=document.querySelector("#tri");
+let cx=x.getContext("2d");
+cx.strokeStyle="white";
+cx.moveTo(10,15);
+cx.lineTo(28.5,52);
+cx.lineTo(45,15);
+cx.moveTo(28,44);
+cx.lineTo(12,12);
+cx.lineTo(43,12);
+cx.moveTo(15.5,15);
+cx.lineTo(41,15);
+cx.stroke();
+
